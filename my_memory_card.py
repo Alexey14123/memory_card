698e57d45f2b8dcfd072b1ae6b88c259f06be31f
Alexey14123 <sigmakriper2727@gmail.com>
2025-05-03 11:08:29 +0300

    Add files via upload
    v1.0

---
+++ my_memory_card.py
@@ -0,0 +1,60 @@
+#создай приложение для запоминания информации
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QLabel, QRadioButton,QGroupBox,QButtonGroup
+from random import randint, shuffle
+
+class Question():
+    def __init__(self, question, right_answer, wrong1, wrong2, wrong3):
+        self.question = question
+        self.right_answer = right_answer
+        self.wrong1 = wrong1
+        self.wrong2 = wrong2
+        self.wrong3 = wrong3
+
+app = QApplication([])
+window = QWidget()
+
+btn_OK = QPushButton('Ответить')
+lb_Question = QLabel('Какой национальности не существует?')
+
+RadioGroupBox = QGroupBox("Варианты ответов")
+rbtn_1 = QRadioButton('Энцы')
+rbtn_2 = QRadioButton('Смурфы')
+rbtn_3 = QRadioButton('Чулымцы')
+rbtn_4 = QRadioButton('Алеуты')
+
+layout_ans1 = QHBoxLayout()
+layout_ans2 = QVBoxLayout()
+layout_ans3 = QVBoxLayout()
+layout_ans2.addWidget(rbtn_1)
+layout_ans2.addWidget(rbtn_2)
+layout_ans3.addWidget(rbtn_3)
+layout_ans3.addWidget(rbtn_4)
+layout_ans1.addLayout(layout_ans2)
+layout_ans1.addLayout(layout_ans3)
+
+RadioGroupBox.setLayout(layout_ans1)
+
+layout_line1 = QHBoxLayout()
+layout_line2 = QHBoxLayout()
+layout_line3 = QHBoxLayout()
+
+layout_line1.addWidget(lb_Question, alignment=(Qt.AlignHCenter | Qt.AlignVCenter))
+layout_line2.addWidget(RadioGroupBox)
+layout_line3.addStretch(1)
+layout_line3.addWidget(btn_OK, stretch=20)
+layout_line3.addStretch(1)
+
+layout_card = QVBoxLayout()
+
+layout_card.addLayout(layout_line1, stretch=2)
+layout_card.addLayout(layout_line2, stretch=8)
+layout_card.addStretch(1)
+layout_card.addLayout(layout_line3, stretch=1)
+layout_card.addStretch(1)
+layout_card.addSpacing(10)
+
+window.setLayout(layout_card)
+window.show()
+app.exec()
+
